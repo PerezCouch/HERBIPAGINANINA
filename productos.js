bcafee5d5a08ef10f2e5cf6dd00f1647944512da
Daniel Sanchez <perezcouch33@gmail.com>
2025-02-07 14:00:48 -0500

    Update Nekroso

---
+++ productos.js
@@ -0,0 +1,17 @@
+// productos.js
+const productos = [
+    // Productos Herbi
+    { name: "Canela y clavo", price: 1200, category: "filtrantes", marca: "herbi", image: "images/products/CANELA-Y-CLAVO-X100.png" },
+    { name: "Anis caja", price: 800, category: "filtrantes", marca: "herbi", image: "images/products/ANIS-X100.jpg" },
+    { name: "Boldo caja", price: 25, category: "filtrantes", marca: "herbi", image: "images/products/BOLDO-X100.jpg" },
+    { name: "Mate de Coca caja", price: 500, category: "filtrantes", marca: "herbi", image: "images/products/MATE-DE-COCA-X100.jpg" },
+    { name: "Relax Té caja", price: 100, category: "filtrantes", marca: "herbi", image: "images/products/RELAX-X100.jpg" },
+    { name: "Durazno caja", price: 40, category: "filtrantes", marca: "herbi", image: "images/products/DURAZNO-X100.jpg" },
+    { name: "Uña de Neko caja", price: 100, category: "filtrantes", marca: "herbi", image: "images/products/UNA-DE-GATO-X100.jpg" },
+    { name: "Naranja caja", price: 100, category: "filtrantes", marca: "herbi", image: "images/products/TE-NARANJA-X100.jpg" },
+    { name: "YoYo Bolsa", price: 100, category: "chocolates", marca: "herbi", image: "images/products/Mesa-de-trabajo-4-100-600x599.jpg" },
+    { name: "Manzanilla Caja", price: 100, category: "filtrantes", marca: "herbi", image: "images/products/MANZANILLA-X100.jpg" },
+    { name: "Relax Té caja", price: 100, category: "filtrantes", marca: "rapidin", image: "images/products/RELAX-X100.jpg" },
+    { name: "Relax Té caja", price: 150, category: "filtrantes", marca: "villa", image: "images/products/RELAX-X100.jpg" },
+    // Productos Rapidin
+];
