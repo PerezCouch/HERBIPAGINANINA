9bbb349c44fcbcb39e3fb7147246d06d581203ac
Daniel Sanchez <perezcouch33@gmail.com>
2025-02-08 13:50:13 -0500

    Big update

--- productos.js
+++ productos.js
@@ -9,7 +9,6 @@ const productos = [
     { name: "Durazno caja", price: 40, category: "filtrantes", marca: "herbi", image: "images/products/DURAZNO-X100.jpg" },
     { name: "Uña de Neko caja", price: 100, category: "filtrantes", marca: "herbi", image: "images/products/UNA-DE-GATO-X100.jpg" },
     { name: "Naranja caja", price: 100, category: "filtrantes", marca: "herbi", image: "images/products/TE-NARANJA-X100.jpg" },
-    { name: "YoYo Bolsa", price: 100, category: "chocolates", marca: "herbi", image: "images/products/Mesa-de-trabajo-4-100-600x599.jpg" },
     { name: "Manzanilla Caja", price: 100, category: "filtrantes", marca: "herbi", image: "images/products/MANZANILLA-X100.jpg" },
     { name: "Relax Té caja", price: 100, category: "filtrantes", marca: "rapidin", image: "images/products/RELAX-X100.jpg" },
     { name: "Relax Té caja", price: 150, category: "filtrantes", marca: "villa", image: "images/products/RELAX-X100.jpg" },
